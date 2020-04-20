508f458360fa58a9ddd0176741c1e55ccb9000c0
Takahisa Sakuma <tsheadbang@gamil.com>
2020-04-20 16:51:17 +0900

    testing

---
+++ pages/cards.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <transition appear name="slide-in">
+      <h1 class="title">Cards example</h1>
+    </transition>
+    <!-- <transition-group appear name="slidIn" class="grid" tag="div"> -->
+    <div class="grid">
+      <div v-for="item in items" :key="item.id" class="card">{{ item }}</div>
+    </div>
+    <!-- </transition-group> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [1, 2, 3, 4, 5]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: #111;
+  min-height: 100vh;
+  color: #fff;
+  text-align: center;
+  // display: flex;
+  // justify-content: center;
+  // flex-direction: column;
+  // align-items: center;
+}
+
+.title {
+  font-size: 3rem;
+  padding: 10px;
+  margin-bottom: 30px;
+}
+
+.card {
+  height: 160px;
+  // width: 200px;
+  background-color: #fff;
+}
+
+.grid {
+  display: grid;
+  grid-gap: 10px;
+  // grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-columns: repeat(2, 1fr);
+}
+
+.slide-in-enter {
+  opacity: 0;
+  transform: scale(0.5);
+}
+.slide-in-enter-active {
+  transition: 0.4s ease;
+}
+</style>
--- pages/vue-anime.vue
+++ pages/vue-anime.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="">
-    <h1>{{ title }}</h1>
+  <div class="vue">
+    <transition appear name="title">
+      <h1 class="title">{{ title }}</h1>
+    </transition>
+
+    <div class="wrapper">
+      <div class="box animated-sample"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import '@/assets/css/animate.css'
+
 export default {
   data() {
     return {
@@ -14,4 +22,64 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// 共通パーツ
+.vue {
+  background-color: #111;
+  min-height: 100vh;
+  color: #fff;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.title {
+  font-size: 3rem;
+  margin: 20px;
+}
+
+.wrapper {
+  margin-bottom: 60px;
+}
+
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: #fff;
+  margin: 0 auto;
+  position: relative;
+  // left: -100%;
+  // opacity: 0;
+}
+// 共通パーツ終わり
+
+.title-enter {
+  opacity: 0;
+  transform: scale(0.5);
+  // transform: translateX(20%);
+}
+.title-enter-active {
+  transition: 0.5s;
+}
+
+.animated-sample {
+  opacity: 0;
+  left: -100%;
+  animation: sample 1.2s forwards;
+}
+
+@keyframes sample {
+  0% {
+    left: -100%;
+    opacity: 0;
+  }
+  90% {
+  }
+  100% {
+    left: 0%;
+    opacity: 1;
+  }
+}
+</style>
