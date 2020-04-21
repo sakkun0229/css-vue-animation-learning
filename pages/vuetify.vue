18601d93f9b59621d7a1debd0922995009de3d3e
Takahisa Sakuma <[tsheadbang@gmail.com]>
2020-04-22 00:17:57 +0900

    enterとleaveがいい感じに動く

--- pages/vuetify.vue
+++ pages/vuetify.vue
@@ -3,8 +3,12 @@
     <v-content>
       <v-container fluid>
         <h1 class="text-center mb-6 ">Vuetify</h1>
+        <div class="text-center mb-4">
+          <v-btn @click="add">add</v-btn>
+          <v-btn @click="remove">remove</v-btn>
+        </div>
         <transition-group appear name="cards" @before-enter="beforeEnter" @after-enter="afterEnter">
-          <v-card class="mx-auto mb-2" color="#26c6da" dark max-width="400" v-for="item in items" :key="item">
+          <v-card class="mx-auto mb-2 card" color="#26c6da" dark max-width="400" v-for="item in items" :key="item">
             <v-card-title>
               <v-icon large left>
                 mdi-twitter
@@ -37,6 +41,12 @@ export default {
     // }
   },
   methods: {
+    add() {
+      this.items.splice(Math.random() * this.items.length, 0, this.items.length + 1)
+    },
+    remove() {
+      this.items.splice(Math.random() * this.items.length, 1)
+    },
     beforeEnter(el) {
       // el.style.transitionDelay = 100 * parseInt(el.dataset.index, 10) + 'ms'
       // console.log(el.style)
@@ -52,12 +62,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.cards-enter {
-  opacity: 0;
-  transform: scale(0.5) translateX(100%);
+.cards {
+  &-enter {
+    opacity: 0;
+    transform: scale(0.5) translateX(100%);
+  }
+  &-leave {
+    &-to {
+      opacity: 0;
+      // transform: scale(0.5) translateX(100%);
+      transform: scale(0.5);
+    }
+    &-active {
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%); //←これを無効で右にスライドしながら消える
+      width: 400px; //幅固定にしないとcardがinline-blockになって横幅縮む
+    }
+  }
 }
-.cards-enter-active,
-.cards-move {
+
+.card {
   transition: 0.5s;
+  // display: inline-block;
 }
 </style>
